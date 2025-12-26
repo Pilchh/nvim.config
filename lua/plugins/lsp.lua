a4f01c8a64e6bee48346867468162fe01a0a759d
Pilchh <alex@pilchh.dev>
2025-12-26 22:11:15 +0000

    Fixed treesitter config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,17 +4,23 @@ return {
 		branch = "master",
 		lazy = false,
 		build = ":TSUpdate",
-		opts = {
-			-- A list of parser names, or "all" (the listed parsers MUST always be installed)
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				-- A list of parser names, or "all" (the listed parsers MUST always be installed)
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
+				-- Install parsers synchronously (only applied to `ensure_installed`)
+				sync_install = false,
 
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = false,
-		},
+				-- Automatically install missing parsers when entering buffer
+				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+				auto_install = false,
+
+				highlight = {
+					enable = true,
+				},
+			})
+		end,
 	},
 	{ -- Autoformat
 		"stevearc/conform.nvim",
@@ -293,7 +299,9 @@ return {
 				-- clangd = {},
 				-- gopls = {},
 				-- pyright = {},
-				-- rust_analyzer = {},
+				rust_analyzer = {
+					filetypes = { "rust" },
+				},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
