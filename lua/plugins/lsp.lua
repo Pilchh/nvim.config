3ecd2b97ce73de4017259bca3c30a171df9aa9c7
Pilchh <alex@thepilchers.com>
2026-01-23 23:28:26 +0000

    Added showing hidden files and enabled hip syntax highlighting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -236,39 +236,39 @@ return {
 					--
 					-- When you move your cursor, the highlights will be cleared (the second autocommand).
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if
-						client
-						and client_supports_method(
-							client,
-							vim.lsp.protocol.Methods.textDocument_documentHighlight,
-							event.buf
-						)
-					then
-						local highlight_augroup =
-							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
-						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.document_highlight,
-						})
-
-						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.clear_references,
-						})
-
-						vim.api.nvim_create_autocmd("LspDetach", {
-							group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-							callback = function(event2)
-								vim.lsp.buf.clear_references()
-								vim.api.nvim_clear_autocmds({
-									group = "kickstart-lsp-highlight",
-									buffer = event2.buf,
-								})
-							end,
-						})
-					end
+					-- if
+					-- 	client
+					-- 	and client_supports_method(
+					-- 		client,
+					-- 		vim.lsp.protocol.Methods.textDocument_documentHighlight,
+					-- 		event.buf
+					-- 	)
+					-- then
+					-- 	local highlight_augroup =
+					-- 		vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+					-- 	vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+					-- 		buffer = event.buf,
+					-- 		group = highlight_augroup,
+					-- 		callback = vim.lsp.buf.document_highlight,
+					-- 	})
+					--
+					-- 	vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+					-- 		buffer = event.buf,
+					-- 		group = highlight_augroup,
+					-- 		callback = vim.lsp.buf.clear_references,
+					-- 	})
+					--
+					-- 	vim.api.nvim_create_autocmd("LspDetach", {
+					-- 		group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+					-- 		callback = function(event2)
+					-- 			vim.lsp.buf.clear_references()
+					-- 			vim.api.nvim_clear_autocmds({
+					-- 				group = "kickstart-lsp-highlight",
+					-- 				buffer = event2.buf,
+					-- 			})
+					-- 		end,
+					-- 	})
+					-- end
 
 					-- The following code creates a keymap to toggle inlay hints in your
 					-- code, if the language server you are using supports them
@@ -347,6 +347,7 @@ return {
 				},
 
 				clangd = {
+					filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "hip" },
 					capabilities = capabilities,
 					completion = {
 						callSnippet = "Disable",
@@ -354,6 +355,13 @@ return {
 					},
 				},
 			}
+
+			-- Treat hip files as cpp
+			vim.filetype.add({
+				extension = {
+					hip = "cpp",
+				},
+			})
 
 			for name, config in pairs(servers) do
 				vim.lsp.enable(name)
